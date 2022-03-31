2c77d8f3b7e57f650032775a40d0b58e378c73a7
Ramil Nugmanov <rnugmano@its.jnj.com>
2022-03-31 17:35:29 +0200

    acid-base enumeration. module refactored.

---
+++ chython/algorithms/tautomers/acid_base.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2022 Ramil Nugmanov <nougmanoff@protonmail.com>
+#  This file is part of chython.
+#
+#  chython is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
+from itertools import combinations, product
+from typing import TYPE_CHECKING, Union, List
+from ._acid import rules as acid_rules, stripped_rules as stripped_acid_rules
+from ._base import rules as base_rules, stripped_rules as stripped_base_rules
+
+
+if TYPE_CHECKING:
+    from chython import MoleculeContainer
+
+
+class AcidBase:
+    __slots__ = ()
+
+    def neutralize(self: 'MoleculeContainer', *, keep_charge=True, logging=False,
+                   _fix_stereo=True) -> Union[bool, List[int]]:
+        """
+        Convert organic salts to neutral form if possible. Only one possible form used for charge unbalanced structures.
+
+        :param keep_charge: do partial neutralization to keep total charge of molecule.
+        :param logging: return changed atoms list.
+        """
+        try:
+            mol, changed = next(self._neutralize(keep_charge))
+        except StopIteration:
+            if logging:
+                return []
+            return False
+
+        self._charges.update(mol._charges)
+        self._hydrogens.update(mol._hydrogens)
+        self.flush_cache()
+        if _fix_stereo:
+            self.fix_stereo()
+        if logging:
+            return list(changed)
+        return True
+
+    def enumerate_charged_forms(self: 'MoleculeContainer', *, deep: int = 4, limit: int = 1000):
+        """
+        Enumerate protonated and deprotonated ions. Use on neutralized molecules.
+
+        :param deep: Maximum amount of added or removed protons.
+        :param limit: Maximum amount of generated structures.
+        """
+        if limit < 1:
+            raise ValueError('limit should be greater or equal 1')
+
+        donors = set()
+        acceptors = set()
+        for q in acid_rules:
+            for mapping in q.get_mapping(self, automorphism_filter=False):
+                donors.add(mapping[1])
+        for q in base_rules:
+            for mapping in q.get_mapping(self, automorphism_filter=False):
+                acceptors.add(mapping[1])
+
+        h_source = [None] * deep + list(donors)
+        h_drain = list(acceptors) + [None] * deep
+
+        seen = set()
+        seen_combo = set()
+        for r in range(min(len(acceptors), deep), 0, -1):  # number of acceptors to protonate
+            for ac in combinations(acceptors, r):  # acceptors atoms to protonate
+                uniq = set()
+                for dc in combinations(h_source, r):  # H donors
+                    if dc in uniq:  # filter None-containing duplicates
+                        continue
+                    uniq.add(dc)
+                    seen_combo.add((dc, ac))
+                    mol = self.copy()
+                    for n in ac:
+                        mol._hydrogens[n] += 1
+                        mol._charges[n] += 1
+                    for n in dc:
+                        if n is not None:
+                            mol._hydrogens[n] -= 1
+                            mol._charges[n] -= 1
+                    if mol in seen:
+                        continue
+                    seen.add(mol)
+                    yield mol
+                    limit -= 1
+                    if not limit:
+                        return
+
+        for r in range(1, min(len(donors), deep) + 1):
+            for dc in combinations(donors, r):
+                uniq = set()
+                for ac in combinations(h_drain, r):  # H acceptors
+                    if ac in uniq:
+                        continue
+                    uniq.add(ac)
+                    if (dc, ac) in seen_combo:
+                        continue
+                    mol = self.copy()
+                    for n in ac:
+                        if n is not None:
+                            mol._hydrogens[n] += 1
+                            mol._charges[n] += 1
+                    for n in dc:
+                        if n is not None:
+                            mol._hydrogens[n] -= 1
+                            mol._charges[n] -= 1
+                    if mol in seen:
+                        continue
+                    seen.add(mol)
+                    yield mol
+                    limit -= 1
+                    if not limit:
+                        return
+
+    def _neutralize(self: 'MoleculeContainer', keep_charge=True):
+        donors = set()
+        acceptors = set()
+        for q in stripped_acid_rules:
+            for mapping in q.get_mapping(self, automorphism_filter=False):
+                donors.add(mapping[1])
+        for q in stripped_base_rules:
+            for mapping in q.get_mapping(self, automorphism_filter=False):
+                acceptors.add(mapping[1])
+
+        if keep_charge:
+            if not donors or not acceptors:
+                return  # neutralization impossible
+            elif len(donors) > len(acceptors):
+                copy = self.copy()
+                for a in acceptors:
+                    copy._hydrogens[a] += 1
+                    copy._charges[a] += 1
+                for c in combinations(donors, len(acceptors)):
+                    mol = copy.copy()
+                    for d in c:
+                        mol._hydrogens[d] -= 1
+                        mol._charges[d] -= 1
+                    yield mol, acceptors.union(c)
+            elif len(donors) < len(acceptors):
+                copy = self.copy()
+                for d in donors:
+                    copy._hydrogens[d] -= 1
+                    copy._charges[d] -= 1
+                for c in combinations(acceptors, len(donors)):
+                    mol = copy.copy()
+                    for a in c:
+                        mol._hydrogens[a] += 1
+                        mol._charges[a] += 1
+                    yield mol, donors.union(c)
+            else:  # balanced!
+                mol = self.copy()
+                for d in donors:
+                    mol._hydrogens[d] -= 1
+                    mol._charges[d] -= 1
+                for a in acceptors:
+                    mol._hydrogens[a] += 1
+                    mol._charges[a] += 1
+                yield mol, donors | acceptors
+        elif donors or acceptors:
+            mol = self.copy()
+            for d in donors:
+                mol._hydrogens[d] -= 1
+                mol._charges[d] -= 1
+            for a in acceptors:
+                mol._hydrogens[a] += 1
+                mol._charges[a] += 1
+            yield mol, donors | acceptors
+
+    def _enumerate_zwitter_tautomers(self: 'MoleculeContainer'):
+        donors = set()
+        acceptors = set()
+        for q in acid_rules:
+            for mapping in q.get_mapping(self, automorphism_filter=False):
+                donors.add(mapping[1])
+        for q in base_rules:
+            for mapping in q.get_mapping(self, automorphism_filter=False):
+                acceptors.add(mapping[1])
+
+        for d, a in product(donors, acceptors):
+            mol = self.copy()
+            mol._hydrogens[d] -= 1
+            mol._hydrogens[a] += 1
+            mol._charges[d] -= 1
+            mol._charges[a] += 1
+            yield mol
+
+
+__all__ = ['AcidBase']
---
+++ chython/algorithms/tautomers/heteroarenes.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2022 Ramil Nugmanov <nougmanoff@protonmail.com>
+#  This file is part of chython.
+#
+#  chython is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
+from collections import deque, defaultdict
+from itertools import product
+from typing import TYPE_CHECKING
+from ..aromatics.kekule import _kekule_component
+from ...exceptions import InvalidAromaticRing
+
+
+if TYPE_CHECKING:
+    from chython import MoleculeContainer
+
+
+class HeteroArenes:
+    __slots__ = ()
+
+    def _enumerate_hetero_arene_tautomers(self: 'MoleculeContainer'):
+        atoms = self._atoms
+        bonds = self._bonds
+        hydrogens = self._hydrogens
+        charges = self._charges
+        radicals = self._radicals
+
+        rings = defaultdict(list)  # aromatic skeleton
+        for n, m_bond in bonds.items():
+            for m, bond in m_bond.items():
+                if bond.order == 4:
+                    rings[n].append(m)
+        if not rings:
+            return
+
+        acceptors = set()
+        donors = set()
+        single_bonded = set()
+        for n, ms in rings.items():
+            if len(ms) == 2:
+                if atoms[n].atomic_number in (5, 7, 15) and not charges[n] and not radicals[n]:
+                    # only neutral B, N, P
+                    if hydrogens[n]:  # pyrrole
+                        donors.add(n)
+                    elif len(bonds[n]) == 2:  # pyridine
+                        acceptors.add(n)
+                    else:
+                        single_bonded.add(n)
+                elif charges[n] == -1 and atoms[n].atomic_number == 6:  # ferrocene
+                    single_bonded.add(n)
+            elif len(ms) == 3 and atoms[n].atomic_number in (5, 7, 15) and not charges[n] and not radicals[n]:
+                single_bonded.add(n)
+        if not donors or not acceptors:
+            return
+
+        atoms = set(rings)
+        components = []
+        while atoms:
+            start = atoms.pop()
+            component = {start: rings[start]}
+            queue = deque([start])
+            while queue:
+                current = queue.popleft()
+                for n in rings[current]:
+                    if n not in component:
+                        queue.append(n)
+                        component[n] = rings[n]
+
+            atoms.difference_update(component)
+            if donors.isdisjoint(component) or acceptors.isdisjoint(component):
+                continue
+            components.append(component)
+
+        if not components:
+            return
+        for component in components:
+            for d, a in product(component.keys() & donors, component.keys() & acceptors):
+                sb = component.keys() & single_bonded
+                sb.add(a)  # now pyrrole
+                try:
+                    next(_kekule_component(component, sb, (), 0))
+                except InvalidAromaticRing:
+                    continue
+                mol = self.copy()
+                mol._hydrogens[d] = 0
+                mol._hydrogens[a] = 1
+                yield mol
+
+
+__all__ = ['HeteroArenes']
