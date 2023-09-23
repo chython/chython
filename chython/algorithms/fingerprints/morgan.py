57595a621acc0765b7409b4e2ff75b96ad68263e
TimurGimadiev <timur.gimadiev@gmail.com>
2023-09-23 12:46:23 +0900

    update morgan with smiles methods

--- chython/algorithms/fingerprints/morgan.py
+++ chython/algorithms/fingerprints/morgan.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #  Copyright 2021-2023 Ramil Nugmanov <nougmanoff@protonmail.com>
+#  Copyright 2023 Timur Gimadiev <timur.gimadiev@gmail.com>
 #  Copyright 2021 Aleksandr Sizov <murkyrussian@gmail.com>
 #  This file is part of chython.
 #
@@ -19,9 +20,9 @@
 #
 from collections import defaultdict
 from math import log2
-from numpy import uint8, zeros
-from typing import Any, TYPE_CHECKING, Set, Union, Dict
+from typing import TYPE_CHECKING, Any, Dict, Set, Union
 
+from numpy import uint8, zeros
 
 if TYPE_CHECKING:
     from chython import MoleculeContainer
@@ -72,32 +73,88 @@ class MorganFingerprint:
                     active_bits.add(tpl & mask)
         return active_bits
 
-    def morgan_hash_set(self: 'MoleculeContainer', min_radius: int = 1, max_radius: int = 4) -> Set[int]:
+    def morgan_smiles_bit(self, min_radius: int = 1, max_radius: int = 4,
+                       length: int = 1024, number_active_bits: int = 2) -> defaultdict[str, set]:
+        """
+        Transform structures into set of indexes of True-valued features.
+
+        :param min_radius: minimal radius of EC
+        :param max_radius: maximum radius of EC
+        :param length: bit string's length. Should be power of 2
+        :param number_active_bits: number of active bits for each hashed tuple
+        """
+        mask = length - 1
+        log = int(log2(length))
+
+        active_bits = defaultdict(set)
+        for smi, tpls in self.morgan_smiles_hash(min_radius, max_radius).items():
+            for tpl in tpls:
+                active_bits[smi].add(tpl & mask)
+                if number_active_bits == 2:
+                    active_bits[smi].add(tpl >> log & mask)
+                elif number_active_bits > 2:
+                    for _ in range(1, number_active_bits):
+                        tpl >>= log
+                        active_bits[smi].add(tpl & mask)
+        return active_bits
+
+    def morgan_hash_set(self: Union["MoleculeContainer"],
+                        min_radius: int = 1,  max_radius: int = 4) -> Set[int]:
         """
         Transform structures into integer hashes of atoms with EC.
 
         :param min_radius: minimal radius of EC
         :param max_radius: maximum radius of EC
         """
-        identifiers = self._atom_identifiers
+        return self.morgan_smiles_hash(min_radius=min_radius, max_radius=max_radius, only_hashes=True)
 
+    def morgan_smiles_hash(self: "MoleculeContainer", min_radius: int = 1, max_radius: int = 4,
+                           only_hashes: bool = False) -> Union[Set[int], defaultdict[Any, list[int]]]:
+        """
+        Transform structures into integer hashes of atoms with EC.
+
+        :param min_radius: minimal radius of EC
+        :param max_radius: maximum radius of EC
+        :param only_hashes: return dict of substructures and hashes or return only set of hashes
+        """
+        identifiers = self._atom_identifiers
         bonds = self._bonds
+        hash2smi = {}
         arr = set()
         for step in range(1, max_radius + 1):
+            tmp_identifiers = {}
             if step >= min_radius:
+                if not only_hashes:
+                    hash2smi.update(self._hash2smi(identifiers, arr, step))
                 arr.update(identifiers.values())
-            identifiers = {idx: hash((tpl, *(x for x in
-                                             sorted((int(b), identifiers[ngb]) for ngb, b in bonds[idx].items())
-                                             for x in x)))
-                           for idx, tpl in identifiers.items()}
-
+            for atom, tpl in identifiers.items():
+                hashes = tuple(x for x in sorted((int(b), identifiers[ngb])
+                                                 for ngb, b in bonds[atom].items()) for x in x)
+                tmp_identifiers.update({atom: hash((tpl, *hashes))})
+            identifiers = tmp_identifiers
         if max_radius > 1:  # add last ring
+            if not only_hashes:
+                hash2smi.update(self._hash2smi(identifiers, arr, max_radius))
             arr.update(identifiers.values())
-        return arr
+        if only_hashes:
+            return arr
+        else:
+            smiles2hash = defaultdict(list)
+            for h, smi in hash2smi.items():
+                smiles2hash[smi].append(h)
+            return smiles2hash
+
+    def _hash2smi(self: "MoleculeContainer", identifiers, arr, radius):
+        hash2smi = {}
+        for atom, h in identifiers.items():
+            if h not in arr:
+                smi = format(self.augmented_substructure((atom,), deep=radius - 1), "A")
+                hash2smi[h] = smi
+        return hash2smi
 
     @property
     def _atom_identifiers(self) -> Dict[int, int]:
         raise NotImplementedError
 
 
-__all__ = ['MorganFingerprint']
+__all__ = ["MorganFingerprint"]
