f6155e1d5d954aa8f1fd686e77684a1d20fbaecc
stsouko <nougmanoff@protonmail.com>
2024-12-22 12:01:12 +0100

    entry point not found. reverted back api

--- chython/algorithms/calculate2d/__init__.py
+++ chython/algorithms/calculate2d/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 #
+#  Copyright 2019-2024 Ramil Nugmanov <nougmanoff@protonmail.com>
 #  Copyright 2024 Denis Lipatov <denis.lipatov163@gmail.com>
 #  Copyright 2024 Vyacheslav Grigorev <slavick2000@yandex.ru>
 #  Copyright 2024 Timur Gimadiev <timur.gimadiev@gmail.com>
+#  Copyright 2019, 2020 Dinar Batyrshin <batyrshin-dinar@mail.ru>
 #  This file is part of chython.
 #
 #  chython is free software; you can redistribute it and/or modify
@@ -18,4 +20,129 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program; if not, see <https://www.gnu.org/licenses/>.
 #
-from .clean2d import Calculate2DMolecule, Calculate2DReaction
+from typing import TYPE_CHECKING, Union
+
+
+if TYPE_CHECKING:
+    from chython import ReactionContainer, MoleculeContainer
+
+
+class Calculate2DMolecule:
+    __slots__ = ()
+
+    def clean2d(self: Union['MoleculeContainer', 'Calculate2DMolecule']):
+        """
+        Calculate 2d layout of graph.
+        https://pubs.acs.org/doi/10.1021/acs.jcim.7b00425 JS implementation used as a reference.
+        """
+        # todo: reimplement
+        self.__dict__.pop('__cached_method__repr_svg_', None)
+
+    def _fix_plane_mean(self: 'MoleculeContainer', shift_x: float, shift_y=0., component=None) -> float:
+        atoms = self._atoms
+        if component is None:
+            component = atoms
+
+        left_atom = atoms[min(component, key=lambda x: atoms[x].x)]
+        right_atom = atoms[max(component, key=lambda x: atoms[x].x)]
+
+        min_x = left_atom.x - shift_x
+        if len(left_atom.atomic_symbol) == 2:
+            min_x -= .2
+
+        max_x = right_atom.x - min_x
+        min_y = min(atoms[x].y for x in component)
+        max_y = max(atoms[x].y for x in component)
+        mean_y = (max_y + min_y) / 2 - shift_y
+        for n in component:
+            a = atoms[n]
+            a._x -= min_x
+            a._y -= mean_y
+
+        if -.18 <= right_atom.y <= .18:
+            factor = right_atom.implicit_hydrogens
+            if factor == 1:
+                max_x += .15
+            elif factor:
+                max_x += .25
+        return max_x
+
+    def _fix_plane_min(self: 'MoleculeContainer', shift_x: float, shift_y=0., component=None) -> float:
+        atoms = self._atoms
+        if component is None:
+            component = atoms
+
+        right_atom = atoms[max(component, key=lambda x: atoms[x].x)]
+        min_x = min(atoms[x].x for x in component) - shift_x
+        max_x = right_atom.x - min_x
+        min_y = min(atoms[x].y for x in component) - shift_y
+
+        for n in component:
+            a = atoms[n]
+            a._x -= min_x
+            a._y -= min_y
+
+        if shift_y - .18 <= right_atom.y <= shift_y + .18:
+            factor = right_atom.implicit_hydrogens
+            if factor == 1:
+                max_x += .15
+            elif factor:
+                max_x += .25
+        return max_x
+
+
+class Calculate2DReaction:
+    __slots__ = ()
+
+    def clean2d(self: 'ReactionContainer'):
+        """
+        Recalculate 2d coordinates
+        """
+        for m in self.molecules():
+            m.clean2d()
+        self.fix_positions()
+
+    def fix_positions(self: 'ReactionContainer'):
+        """
+        Fix coordinates of molecules in reaction
+        """
+        shift_x = 0
+        reactants = self.reactants
+        amount = len(reactants) - 1
+        signs = []
+        for m in reactants:
+            max_x = m._fix_plane_mean(shift_x)
+            if amount:
+                max_x += .2
+                signs.append(max_x)
+                amount -= 1
+            shift_x = max_x + 1
+        arrow_min = shift_x
+
+        if self.reagents:
+            shift_x += .4
+            for m in self.reagents:
+                max_x = m._fix_plane_min(shift_x, .5)
+                shift_x = max_x + 1
+            shift_x += .4
+            if shift_x - arrow_min < 3:
+                shift_x = arrow_min + 3
+        else:
+            shift_x += 3
+        arrow_max = shift_x - 1
+
+        products = self.products
+        amount = len(products) - 1
+        for m in products:
+            max_x = m._fix_plane_mean(shift_x)
+            if amount:
+                max_x += .2
+                signs.append(max_x)
+                amount -= 1
+            shift_x = max_x + 1
+        self._arrow = (arrow_min, arrow_max)
+        self._signs = tuple(signs)
+        self.flush_cache()
+
+
+__all__ = ['Calculate2DMolecule', 'Calculate2DReaction']
